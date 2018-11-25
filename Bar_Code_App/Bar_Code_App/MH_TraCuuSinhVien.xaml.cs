20157cbfa9d59f2a6720155a07b4ab61504ba6f7
TamLNM <minhtam2996@gmail,com>
2018-11-25 18:17:12 +0700

    [TamLNM] Initial code

---
+++ Bar_Code_App/Bar_Code_App/MH_TraCuuSinhVien.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ZXing.Net.Mobile.Forms;
+
+namespace Bar_Code_App
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+
+	public partial class MH_TraCuuSinhVien : ContentPage
+	{
+        ZXingScannerPage scanPage;
+
+        public MH_TraCuuSinhVien ()
+		{
+			InitializeComponent ();
+
+            btnScanDefault.Clicked += btnScanDefault_ClickedAsync;
+        }
+
+        // Add btnScanDefault click event
+        private async void btnScanDefault_ClickedAsync(object sender, EventArgs e)
+        {
+            scanPage = new ZXingScannerPage();
+            scanPage.OnScanResult += (result) =>
+            {
+                scanPage.IsScanning = false;
+
+                // Do ... with result
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    Navigation.PopModalAsync();
+                    DisplayAlert("Scanned Barcode", result.Text, "OK");
+                });
+            };
+            await Navigation.PushModalAsync(scanPage);
+        }
+
+        private void btn_TraCuuTheoMSSV_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new MH_TraCuuTheoMSSV());
+        }
+    }
+}
---
+++ Bar_Code_App/Bar_Code_App/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace Bar_Code_App
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_DanhSachSinhVien_Clicked(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btn_ThemSinhVien_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new MH_ThemThongTinSinhVien());
+        }
+
+        private void btn_TraCuuSinhVien_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new MH_TraCuuSinhVien());
+        }
+    }
+}
